8e9f76f16ea37827c6dfb2576ca7ed0fb40cc129
emimar <emimar1@gmail.com>
2015-07-13 14:02:59 +0200

    Créditos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,17 +1,15 @@
 //GET /quizes/question
-exports.question = function(req, res)
-    {
+exports.question = function(req, res){
     res.render('quizes/question', {pregunta: 'Capital de Italia'});
-    };
+};
  //GET /quizes/answer
- exports.answer = function(req, res)
-    {
+ exports.answer = function(req, res){
     if (req.query.respuesta === 'Roma')
-    	{
-		res.render('quizes/answer', {respuesta: 'Correcto'});
-		}
+    	res.render('quizes/answer', {respuesta: 'Correcto'});
 	else
-		{
 		res.render('quizes/answer', {respuesta: 'Incorrecto'});
-		}
+};
+//GET /author
+exports.author = function(req, res){
+	res.render('author', {autor: 'Mª José Navarro Carrasco'});
 };
